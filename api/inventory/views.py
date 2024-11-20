ee84909af2c24aaff448e6123862f78e78850195
hiroya-akiba <hiroya0504@gmail.com>
2024-11-20 13:34:27 +0000

    2024/11/20 Serverアクセスできないバグを修正(VSCodeのアップデート起因だった) → シリアライズしたレスポンスを確認

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -12,6 +12,17 @@ class ProductView(APIView):
         """
         商品の一覧を取得する
         """
-        queryset = Product.objects.all()
+        # querysetはデータベースからのオブジェクトのコレクションを表す。
+        # 0以上のフィルターを含めて絞り込むことができる。
+        # 例1 全件取得 .all(): select * from Product;
+        queryset = Product.objects.all() 
+        # 例2 額1000円以上 filter(price__gt=1000): select * from Product where price > 1000;
+        queryset = Product.objects.filter(price__gt=1000)
+        # 例3 金額昇順 order_by('price'): select * from product order by price asc;
+        queryset = Product.objects.order_by('price')
+        # 例4 項目指定 values('name','price'): select name, price from product;
+        queryset = Product.objects.values('name', 'price')
+        # その他はDjango公式ドキュメントを参照せよ
+        
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
