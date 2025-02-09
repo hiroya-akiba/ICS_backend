e14d33c7ebfea1bff64265f24d483c2591d35646
hiroya-akiba <hiroya0504@gmail.com>
2025-02-09 23:09:13 +0900

    2025/02/09 DMLファイルのコメント部分を修正

--- api/inventory/migrations/0011_dml_insert_product_data_by_fixture_environment.py
+++ api/inventory/migrations/0011_dml_insert_product_data_by_fixture_environment.py
@@ -7,6 +7,11 @@ def load_fixture(apps, schema_editor):
     common_load_fixture(__file__) # この関数が実行されている絶対パスが__file__に代入され、
     # common_load_fixtureの実装で、settingsファイルによる分別が行われるため、
     # 環境によって読み込むfixtureファイルを変えることが可能
+    # 条件として、settingsファイルのファイル名と、fixtureのファイル名を揃えることが必要
+    # 例) config/settings/product.py を使用してmigrationを行う場合
+    #     (python manage.py migration --settings config.settings.product)
+    #     は、0011_dml_insert_product_data_by_fixture_environment/product.yaml
+    #     が読み込まれる。
 
 class Migration(migrations.Migration):
 
